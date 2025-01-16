a38a65ad5bbe6859ea16614d4bdf49f64b218388
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2025-01-16 15:48:41 +0600

    Initial commit of the project

---
+++ src/main/java/com/example/It_Sphere/ItSphereApplication.java
@@ -0,0 +1,13 @@
+package com.example.It_Sphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/It_Sphere/ItSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.It_Sphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
